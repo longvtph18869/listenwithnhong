ade3d566ea1b844b901e15436d74ba34e5f22e64
longvtph18869 <longvutien15092000@gmail.com>
2023-11-22 09:56:27 +0700

    commit

--- src/components/Player/Control/TrackInfo.tsx
+++ src/components/Player/Control/TrackInfo.tsx
@@ -1,24 +1,33 @@
 import React from "react"
-import { useAppSelector } from "../../../hooks/redux"
+import { useAppDispatch, useAppSelector } from "../../../hooks/redux"
 import { Link } from "react-router-dom"
+import { setOpenLyric } from "../../../redux/features/audioSlice"
 
 const TrackInfo: React.FC = () => {
 
   const info = useAppSelector((state) => state.audio.infoSongPlayer)
+  const isLyrics = useAppSelector((state) => state.audio.isLyric)
+  const dispatch = useAppDispatch()
 
+  const handleOpenLyrics = () => {
+    isLyrics
+    ? dispatch(setOpenLyric(false))
+    : dispatch(setOpenLyric(true))
+  }
   return(
     <div className="flex items-center col-span-2 md:col-span-1">
       {/* Thumbnail */}
       <img
+        onClick={ handleOpenLyrics }
         src={info.thumbnail}
         alt={info.title}
-        className="h-[46px] rounded-[5px]"
+        className="h-[46px] rounded-[5px] cursor-pointer"
       />
       {/* End Thumbnail */}
 
       {/* Info */}
       <div className="flex flex-col justify-center h-[46px] ml-3">
-        <div className="w-[150px] font-semibold text-base text-[color:var(--color-text)] opacity-90 mb-1 truncate cursor-default">{info.title}</div>
+        <div onClick={ handleOpenLyrics } className="w-[150px] font-semibold text-base text-[color:var(--color-text)] opacity-90 mb-1 truncate cursor-pointer">{info.title}</div>
         <div className="w-[150px] flex text-[color:var(--color-text)] text-xs opacity-60 truncate">
         {
           info.artists &&
